76ac23e79b4a27908b895817b425226c779ed543
Ellen Qian <62158505+lnsq@users.noreply.github.com>
2020-08-18 14:48:02 -0500

    Changed comments

--- coin_game.py
+++ coin_game.py
@@ -4,42 +4,14 @@
 ## Context: Problem statement taken from
 ##          Cornell's CS2110 Fall 2014 course, problem 8
 ## https://www.cs.cornell.edu/courses/cs2110/2014fa/L07-Recursion/recursion_practice.pdf
-## Problem statement (for convenience):
-##       Coin game: Alice and Bob are playing a game
-##       using a bunch of coins. The players pick several coins out of the
-##       bunch in turn. Each time a player is allowed to pick 1, 2 or 4 coins,
-##       and the player that gets the last coin is the winner. Assume that
-##       both players are very smart and he/she will try his/her best to
-##       work out a strategy to win the game. For example, if there are 2
-##       coins and Alice is the first player to pick, she will definitely pick 2
-##       coins and win. If there are 3 coins and Alice is still the first player
-##       to pick, no matter she picks 1 or 2 coins, Bob will get the last coin
-##       and win the game. Given the number of coins and the order of
-##       players (which means the first and the second players to pick the
-##       coins), you are required to write a program to calculate the winner
-##       of the game, and calculate how many different strategies there are
-##       for he/she to win the game. You should use recursion to solve
-##       the problem, and the parameters are read from the command line.
-##       You can assume that there are no more than 30 coins.
-## Sample output:
-##        ./pickcoin 1 alice bob
-##        alice 1
-##        ./pickcoin 2 bob alice
-##        bob 1
-##        ./pickcoin 3 alice bob
-##        bob 2
-##        ./pickcoin 10 alice bob
-##        alice 22
-##        ./pickcoin 25 alice bob
-##        alice 3344
-##        ./pickcoin 30 alice bob
-##        bob 18272
 ## Steps:
 ##      1. Input the number of coins you would like the game to have
 ##      2. Input two names in the order in which they will play
 ## Output: The name of the winner followed by the number of different
 ##         strategies the winner could have used to win
-## Notes: Deceptively tough to figure out what the problem statement considered a
+## Notes: Initially seemed like a straightforward problem, but had some tricky spots,
+##        especially when it came to counting and figuring out the correct strategies.
+##        It was deceptively tough to figure out what the problem statement considered a
 ##        "winning strategy" but very satisfying to finally figure out! Took a lot
 ##        of drawing trees.
 
